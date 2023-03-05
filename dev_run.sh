5f5c264bde9cacc19a45a905ebc802222bf3c31e
Konstantin Utenkov <dev@cloudlocalnet.com>
2023-03-05 14:37:17 +0000

    add commented ssl for agent

--- dev_run.sh
+++ dev_run.sh
@@ -8,3 +8,4 @@ mkdir -p /mnt/host
 pip install -r requirements.txt
 
 uvicorn app.api:app --reload --host 0.0.0.0 --port 7190
+#uvicorn app.api:app --reload --host 0.0.0.0 --port 7190 --ssl-keyfile /root/key.pem --ssl-certfile /root/cert.pem
